dab893021ce131a537718dd40a82c460b42d95a0
Nikolaj Schumacher <git@nschum.de>
2013-01-04 22:24:53 +0100

    List compatibility with Emacs 24.

--- highlight-symbol.el
+++ highlight-symbol.el
@@ -6,7 +6,7 @@
 ;; Version: 1.1
 ;; Keywords: faces, matching
 ;; URL: http://nschum.de/src/emacs/highlight-symbol/
-;; Compatibility: GNU Emacs 22.x, GNU Emacs 23.x
+;; Compatibility: GNU Emacs 22.x, GNU Emacs 23.x, GNU Emacs 24.x
 ;;
 ;; This file is NOT part of GNU Emacs.
 ;;
