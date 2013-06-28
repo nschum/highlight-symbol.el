ae1527c76035d1ef2c8783db267b262424f691f5
Nikolaj Schumacher <git@nschum.de>
2013-06-28 15:47:25 +0200

    Fix syntax error in example.

--- highlight-symbol.el
+++ highlight-symbol.el
@@ -30,7 +30,7 @@
 ;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
 ;; (global-set-key [f3] 'highlight-symbol-next)
 ;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
-;; (global-set-key [(meta f3)] 'highlight-symbol-prev)))
+;; (global-set-key [(meta f3)] 'highlight-symbol-prev)
 ;; (global-set-key [(control meta f3)] 'highlight-symbol-query-replace)
 ;;
 ;; Use `highlight-symbol-at-point' to toggle highlighting of the symbol at
