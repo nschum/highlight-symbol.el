0be7f1af1c1d387f518b10a203090db83036f8f6
Nikolaj Schumacher <git@nschum.de>
2013-06-28 15:52:04 +0200

    #9: Make example bindings less likely to conflict.

--- highlight-symbol.el
+++ highlight-symbol.el
@@ -30,8 +30,7 @@
 ;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
 ;; (global-set-key [f3] 'highlight-symbol-next)
 ;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
-;; (global-set-key [(meta f3)] 'highlight-symbol-prev)
-;; (global-set-key [(control meta f3)] 'highlight-symbol-query-replace)
+;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 ;;
 ;; Use `highlight-symbol-at-point' to toggle highlighting of the symbol at
 ;; point throughout the current buffer.  Use `highlight-symbol-mode' to keep the
